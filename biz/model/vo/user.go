dca7e0fc62e3988d9a20d7d6016fc9fc60253989
FeiWuSama <1825578184@qq.com>
2026-02-05 23:28:03 +0800

    fix: 修改返回userVo的属性命名为驼峰格式

--- biz/model/vo/user.go
+++ biz/model/vo/user.go
@@ -4,11 +4,11 @@ import "time"
 
 type UserVo struct {
 	ID          int64     `json:"id,string"`
-	UserAccount string    `json:"user_account"`
-	UserName    string    `json:"user_name"`
-	UserAvatar  string    `json:"user_avatar"`
-	UserProfile string    `json:"user_profile"`
-	UserRole    string    `json:"user_role"`
-	CreateTime  time.Time `json:"create_time"`
-	UpdateTime  time.Time `json:"update_time"`
+	UserAccount string    `json:"userAccount"`
+	UserName    string    `json:"userName"`
+	UserAvatar  string    `json:"userAvatar"`
+	UserProfile string    `json:"userProfile"`
+	UserRole    string    `json:"userRole"`
+	CreateTime  time.Time `json:"createTime"`
+	UpdateTime  time.Time `json:"updateTime"`
 }
