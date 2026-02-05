60b335b82a9dba422cfd177042172e986c59c256
FeiWuSama <1825578184@qq.com>
2026-02-05 13:48:10 +0800

    fix: 修复权限校验赋值枚举值失败的bug

--- biz/middleware/auth.go
+++ biz/middleware/auth.go
@@ -16,13 +16,10 @@ import (
 // AuthMiddleware 鉴权中间件
 func AuthMiddleware(roleEnum enum.UserRoleEnum) app.HandlerFunc {
 	return func(ctx context.Context, c *app.RequestContext) {
-		// 1. 获取用户角色枚举值
-		role := enum.RoleTextMap[roleEnum]
-
-		// 2. 校验权限
+		// 1. 校验权限
 		var userJson []byte
-		if role == enum.UserRole {
-			// 3. 校验Cookie是否存在
+		if roleEnum == enum.UserRole {
+			// 2. 校验Cookie是否存在
 			userJson = c.Request.Header.Cookie(enum.UserLoginState)
 			if userJson == nil {
 				c.JSON(200, pkg.NotLoginError)
@@ -31,7 +28,7 @@ func AuthMiddleware(roleEnum enum.UserRoleEnum) app.HandlerFunc {
 			}
 		}
 
-		// 4. 解析Cookie中的用户信息
+		// 3. 解析Cookie中的用户信息
 		decodedUserJson, err := url.QueryUnescape(string(userJson))
 		if err != nil {
 			c.JSON(200, pkg.NotAuthError)
@@ -47,14 +44,14 @@ func AuthMiddleware(roleEnum enum.UserRoleEnum) app.HandlerFunc {
 			return
 		}
 
-		// 5. 校验用户权限等级是否符合要求
+		// 4. 校验用户权限等级是否符合要求
 		dbUser, err := query.Use(dal.DB).User.Where(query.User.ID.Eq(user.ID), query.User.IsDelete.Eq(0)).First()
 		if err != nil {
 			c.JSON(200, pkg.NotAuthError)
 			c.Abort()
 			return
 		}
-		if role == enum.AdminRole && dbUser.UserRole != role {
+		if roleEnum == enum.AdminRole && enum.UserRoleEnum(dbUser.UserRole) != roleEnum {
 			c.JSON(200, pkg.NotAuthError)
 			c.Abort()
 			return
